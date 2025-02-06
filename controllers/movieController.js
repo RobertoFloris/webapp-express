9ebcf29aa020ae7c0cf433c2221e2d36d79eb047
RobertoFloris <florisroberto99@gmail.com>
2025-02-06 16:40:54 +0100

    [add] controller and router

---
+++ controllers/movieController.js
@@ -0,0 +1,13 @@
+const index = (req, res) => {
+  res.send("Elenco dei film")
+}
+
+const show = (req, res) => {
+  const id = req.params.id
+  res.send(`film con id ${id}`)
+}
+
+module.exports = {
+  index,
+  show
+}
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-
-require('dotenv').config();
-
-
-const port = process.env.PORT || 3000;
-
+const routerMovie = require('./router/movie')
 const errorsHandler = require('./middlewares/errorsHandler');
 const notFound = require('./middlewares/notFound');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'))
@@ -15,6 +13,8 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.send('Server dei film')
 })
+
+app.use('/api/movies', routerMovie);
 
 app.use(errorsHandler);
 app.use(notFound);
