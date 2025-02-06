9f129e5d0b49291c30820a8118b64aec328cccd3
RobertoFloris <florisroberto99@gmail.com>
2025-02-06 16:22:01 +0100

    [add] middlewares

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 const express = require('express');
-
 const app = express();
 
+require('dotenv').config();
+
+
 const port = process.env.PORT || 3000;
+
+const errorsHandler = require('./middlewares/errorsHandler');
+const notFound = require('./middlewares/notFound');
+
+app.use(express.json());
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('Server dei film')
+})
+
+app.use(errorsHandler);
+app.use(notFound);
+
+app.listen(port, () => {
+  console.log(`Sono in ascolto alla porta ${port}`);
+})
