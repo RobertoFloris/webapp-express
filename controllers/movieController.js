14bc84a67ddf79413ec742865c940b259c4fadfb
RobertoFloris <florisroberto99@gmail.com>
2025-02-06 18:19:04 +0100

    [fix] show in movieController.js

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -11,8 +11,51 @@ const index = (req, res) => {
 }
 
 const show = (req, res) => {
-  const id = req.params.id
-  res.send(`film con id ${id}`)
+  const id = req.params.id;
+  const sql =
+    ` 
+  SELECT movies.*, 
+  reviews.id AS reviews_id, reviews.name AS reviews_name, reviews.vote AS reviews_vote, reviews.text AS reviews_text
+  FROM movies
+  LEFT JOIN reviews ON reviews.movie_id = movies.id
+  WHERE movies.id = ?
+  `;
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: "Query errata" })
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ error: 'Post non trovato' });
+    }
+
+    const movie = {
+      id: results[0].id,
+      title: results[0].title,
+      director: results[0].director,
+      genre: results[0].genre,
+      release_year: results[0].release_year,
+      abstract: results[0].abstract,
+      image: results[0].image,
+      created_at: results[0].created_at,
+      updated_at: results[0].updated_at,
+      reviews: []
+    }
+
+    results.forEach(item => {
+      movie.reviews.push({
+        id: item.reviews_id,
+        name: item.reviews_name,
+        vote: item.reviews_vote,
+        text: item.reviews_text
+      })
+    })
+
+    res.json(movie)
+
+  })
+
 }
 
 module.exports = {
